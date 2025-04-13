5dba75a76efee786705f221feb85c64b85a99bfd
bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-04-13 17:45:30 +0530

    Added in intermediate of project

---
+++ app.js
@@ -0,0 +1,107 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const Listing=require("./models/listing.js");
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");// helps to create layout
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+      
+main().then(()=>{
+    console.log("connected to Db");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+//for view folder
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+//api started
+app.get("/",(req,res)=>{
+    res.send("Hi, I am root");
+})
+
+//index route
+app.get("/listings",async(req,res)=>{
+   const allListing= await Listing.find({});
+   res.render("listings/index.ejs",{allListing});
+    
+
+});
+//new route
+app.get("/listings/new",async(req,res)=>{
+    res.render("listings/new.ejs")
+});
+
+//show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+//create route
+app.post("/listings",async(req,res)=>{
+    // let {title,description,image,price,country,location}=req.body;
+    let newListing=new Listing(req.body.listing);
+    await newListing.save();
+ res.redirect("/listings");
+});
+
+//Edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let { id }=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+
+});
+
+//update route
+app.put("/listing/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+//testing
+// app.get("/testListing",async(req,res)=>{
+//     // console.log("testing successfully");
+//     let sampleTesting=new Listing({
+//         title:"my work villa",
+//         description:"ready ta enjoy pai",
+//         image:"shubham-dhage-fMCHQ2uUzrk-unsplash.jpg",
+        
+//         price:12000,
+//         location:"bhubaneswar",
+//         country:"Odisha",
+
+//     })
+//    await sampleTesting.save();
+//    console.log("saved");
+//    res.send("saved successfully");
+// });
+
+
+
+app.listen(8080,()=>{
+    console.log("server is running on port 8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+       
+        type:String,
+        // required:true,
+        default:"shubham-dhage-fMCHQ2uUzrk-unsplash.jpg",
+        set:(v)=>v===""?"shubham-dhage-fMCHQ2uUzrk-unsplash.jpg":v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing=mongoose.model("listing",listingSchema);
+module.exports=Listing;
