7fb65551c70d274717f6a5ca6f21fcd5de9db39f
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-04-22 15:08:50 +0530

    ExpressError added

--- app.js
+++ app.js
@@ -59,6 +59,9 @@ app.get("/listings/:id",wrapAsync(async(req,res)=>{
 //create route
 app.post("/listings",wrapAsync(async(err,req,res,next)=>{
     // let {title,description,image,price,country,location}=req.body;
+    if(!req.body.listing){
+        throw new ExpressError(400,"bad request")
+    }
    
     let newListing=new Listing(req.body.listing);
     await newListing.save();
@@ -76,17 +79,20 @@ app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
 
 //update route
 app.put("/listing/:id",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"bad request")
+    }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
 }));
 //delete route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 //testing
 // app.get("/testListing",async(req,res)=>{
 //     // console.log("testing successfully");
@@ -110,9 +116,10 @@ app.delete("/listings/:id",async(req,res)=>{
 // });
 
 app.use((err,req,res,next)=>{
-    let {statusCode,message}=err;
+    let {statusCode=500,message="something went wrong"}=err;
     // res.send("Something went wrong!");
     res.status(statusCode).send(message);
+    
 });
 
 
