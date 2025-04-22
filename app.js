07e457799d73ef64755a1039ed20a8952c596530
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-04-22 14:45:39 +0530

    `Added ExpressError handling and wrapped async routes with wrapAsync utility`

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");// helps to create layout
 const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError =require("./utils/ExpressError.js");
+
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
       
@@ -36,23 +38,23 @@ app.get("/",(req,res)=>{
 })
 
 //index route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
    const allListing= await Listing.find({});
    res.render("listings/index.ejs",{allListing});
     
 
-});
+}));
 //new route
-app.get("/listings/new",async(req,res)=>{
+app.get("/listings/new",wrapAsync(async(req,res)=>{
     res.render("listings/new.ejs")
-});
+}));
 
 //show route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 //create route
 app.post("/listings",wrapAsync(async(err,req,res,next)=>{
@@ -65,19 +67,19 @@ app.post("/listings",wrapAsync(async(err,req,res,next)=>{
 ));
 
 //Edit route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let { id }=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
 
-});
+}));
 
 //update route
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 //delete route
 app.delete("/listings/:id",async(req,res)=>{
     let {id}=req.params;
@@ -102,8 +104,15 @@ app.delete("/listings/:id",async(req,res)=>{
 //    console.log("saved");
 //    res.send("saved successfully");
 // });
+
+// app.all("*",(req,res,next)=>{
+//     next(new ExpressError(404,"Page not found")); //add expressError
+// });
+
 app.use((err,req,res,next)=>{
-    res.send("Something went wrong!");
+    let {statusCode,message}=err;
+    // res.send("Something went wrong!");
+    res.status(statusCode).send(message);
 });
 
 
