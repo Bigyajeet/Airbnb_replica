d8c7b269e362ea36b94bb8cda1dbd1bc2b0b344b
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-05-25 15:46:49 +0530

    tried cookies signed and unsigned

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");// helps to create layout
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError =require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+const {listingSchema,reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
+const routes=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
 
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
@@ -22,7 +24,7 @@ main().then(()=>{
 async function main() {
   await mongoose.connect(MONGO_URL);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  
 }
 
 //for view folder
@@ -39,104 +41,16 @@ app.get("/",(req,res)=>{
     res.send("Hi, I am root");
 });
 
-//middleware validate using joi
-const validateListing=(req,res,next)=>{
-    let {error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next()
-    }
-};
 
-//index route
-app.get("/listings",wrapAsync(async(req,res)=>{
-   const allListing= await Listing.find({});
-   res.render("listings/index.ejs",{allListing});
-    
 
-}));
-//new route
-app.get("/listings/new",wrapAsync(async(req,res)=>{
-    res.render("listings/new.ejs")
-}));
 
-//show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-}));
 
-//create route
-app.post("/listings", validateListing,wrapAsync(async(err,req,res,next)=>{
-    // let {title,description,image,price,country,location}=req.body;
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"bad request")
-    // }
-    //adding of joi
-    // let result=listingSchema.validate(req.body);
-    // console.log(result);
-    // if(result.error){
-    //     throw new ExpressError(400,result.error);//joi sends error 
-    // }
-    // if(!newListing.title){
-    //     throw new ExpressError(400,"title is missing")
+//resstructering listing
+app.use("/listings",routes);
 
-    // }
-    // if(!newListing.description){
-    //     throw new ExpressError(400,"description is missing")
 
-    // }
-    // if(!newListing.location){
-    //     throw new ExpressError(400,"location is missing")
-
-    // } instead of these if condition we use joi tool for validation 
-
-    
-    await newListing.save();
-     res.redirect("/listings");
-    }
-));
-
-//Edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-    let { id }=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-
-}));
-
-//update route
-app.put("/listing/:id", validateListing,wrapAsync(async(req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"bad request")
-    }
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}));
-//delete route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    let deletedListing=await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-//review for show
-app.post("/listings/:id/reviews",async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    console.log("new review saved");
-    res.send("new review saved");
-});
-
+//restructuring review
+app.use("/listings/:id/reviews",reviews);
 
 //testing
 // app.get("/testListing",async(req,res)=>{
@@ -167,9 +81,6 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render("error.ejs",{message});
     
 });
-
-
-
 
 app.listen(8080,()=>{
     console.log("server is running on port 8080");
