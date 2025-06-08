d01685c9b403e56067c60d7bca60d9434aaf4c97
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-06-09 01:17:55 +0530

    added mvc for review and users

--- routes/review.js
+++ routes/review.js
@@ -7,7 +7,7 @@ const Listing=require("../models/listing.js");
 
 const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
 
-
+const reviewcontroller=require("../controllers/review.js")
 
 
 
@@ -17,19 +17,7 @@ router.post(
     "/",
     isLoggedIn,
     validateReview,
-    wrapAsync(async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-    newReview.author=req.user._id;
-    console.log(newReview);
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-    req.flash("success","New Review Created!")
-   res.redirect(`/listings/${listing._id}`);
- 
-})
+    wrapAsync(reviewcontroller.createReview)
 );
 
 
@@ -38,14 +26,7 @@ router.delete(
     "/:reviewId",
     isLoggedIn,
     isReviewAuthor,
-    wrapAsync(async(req,res)=>{
-        let {id,reviewId}=req.params;
-
-        await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success","Review Deleted!")
-        res.redirect(`/listings/${id}`);
-    })
-);
+    wrapAsync(reviewcontroller.destroyreview
+));
 
 module.exports=router;
