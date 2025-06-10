df76670d1a6526ca133b2c8a539dcf78b4535362
Bigyajeet <bigyajeetkumarpatra@gmail.com>
2025-06-11 02:13:44 +0530

    added image upload features and manipulated form to store in uploads folder

--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,15 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const Listing=require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
 const listingcontroller=require("../controllers/listing.js");
+const multer=require("multer");
+const upload=multer({dest:"uploads/"});
 //index route //create route
 router.route("/")
 .get(wrapAsync(listingcontroller.index))
-.post(isLoggedIn,validateListing,wrapAsync(listingcontroller.createListing));
+// .post(isLoggedIn,validateListing,wrapAsync(listingcontroller.createListing));
+.post(upload.single('listing[image]'),(req,res)=>{
+res.send(req.file);
+});
 
 //new route
 router.get("/new",isLoggedIn,wrapAsync(listingcontroller.newlisting));
